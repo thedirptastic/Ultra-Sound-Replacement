26f03bf52fa8fe31f368c4b2e1b80f00bc1a8aa9
thedirptastic <80712226+thedirptastic@users.noreply.github.com>
2024-06-11 12:57:51 -0700

    First Jackhammer commit

--- UK Sound Replacement/Patches/Shotgun Patches.cs
+++ UK Sound Replacement/Patches/Shotgun Patches.cs
@@ -1,5 +1,7 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using UnityEngine;
+using System.Reflection;
+using System.Linq;
 
 [HarmonyPatch(typeof(Shotgun), "Start")]
 public static class Inject_ShotgunSounds
@@ -57,5 +59,53 @@ public static class Inject_ShotgunPump2Sounds
     {
         SoundPackController.SetAudioClip(ref __instance.pump2sound, "ShotgunPump2", SoundPackController.SoundPackType.Shotgun);
         return true;
+    }
+}
+
+// Jackhamer
+
+[HarmonyPatch(typeof(ShotgunHammer), "Awake")]
+public static class Inject_ShotgunHammerSounds
+{
+    public static string alt = "JH";
+
+    public static void Postfix(ShotgunHammer __instance)
+    {
+        // Access private fields using reflection
+        var pump1Sound = typeof(ShotgunHammer).GetField("pump1Sound", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance) as AudioSource;
+        Debug.LogError("why");
+        var pump2Sound = typeof(ShotgunHammer).GetField("pump2Sound", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance) as AudioSource;
+        Debug.LogError("why2");
+        var pumpExplosion = typeof(ShotgunHammer).GetField("pumpExplosion", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance) as GameObject;
+        Debug.LogError("why3");
+        var overPumpExplosion = typeof(ShotgunHammer).GetField("overPumpExplosion", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance) as GameObject;
+        Debug.LogError("why4");
+        var tempChargeSound = typeof(ShotgunHammer).GetField("tempChargeSound", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance) as GameObject;
+        Debug.LogError("???");
+
+        SoundPackController.SetAudioSourceClip(pump1Sound, "ShotgunPump1" + alt, SoundPackController.SoundPackType.Shotgun);
+        Debug.LogError("define1");
+        SoundPackController.SetAudioSourceClip(pump2Sound, "ShotgunPump2" + alt, SoundPackController.SoundPackType.Shotgun);
+        Debug.LogError("define2");
+        SoundPackController.SetAudioSourceClip(pumpExplosion.GetComponent<AudioSource>(), "PumpExplosion" + __instance.variation + alt, SoundPackController.SoundPackType.Shotgun);
+        Debug.LogError("define3");
+        SoundPackController.SetAudioSourceClip(overPumpExplosion.GetComponent<AudioSource>(), "OverCharged" + alt, SoundPackController.SoundPackType.Shotgun);
+        Debug.LogError("define4");
+        //SoundPackController.SetAudioSourceClip(tempChargeSound.GetComponent<AudioSource>(), "ShotgunChargeLoop" + __instance.variation + alt, SoundPackController.SoundPackType.Shotgun);
+        Debug.LogError("define5");
+
+        /*AudioSource heatSinkAud = (AudioSource)Traverse.Create(__instance).Field("overheatAud").GetValue();
+        if (heatSinkAud == null)
+            heatSinkAud = heatSinkSMR.GetComponent<AudioSource>();
+        SoundPackController.SetAudioSourceClip(heatSinkAud, "HeatHiss" + alt, SoundPackController.SoundPackType.Shotgun);*/
+    }
+}
+
+[HarmonyPatch(typeof(ShotgunHammer), "Update")]
+public static class Inject_ShotgunHammer
+{
+    public static void Postfix(ShotgunHammer __instance)
+    {
+        
     }
 }
